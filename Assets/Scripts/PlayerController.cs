ca9ac8d1981f9da0be98f69cb2ef70d948dddce9
JP <jphil.provost@outlook.com>
2024-01-29 17:58:47 -0500

    Class 4 - Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
 
     [SerializeField] float moveSpeed = 1;
+    [SerializeField] Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +19,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         rb.velocity = new Vector3(
             InputManager.movementInput.x * moveSpeed, 
             rb.velocity.y, 
             InputManager.movementInput.y * moveSpeed);
 
+        UpdateAnimation();
+    }
+    
+    private void UpdateAnimation()
+    {
+        animator.SetFloat("xSpeed", InputManager.movementInput.x);
+        animator.SetFloat("zSpeed", InputManager.movementInput.y);    
     }
 }
